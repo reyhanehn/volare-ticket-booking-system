610deb059aaac9844096777ac870772105c5e8cd
HedieTahmouresi <tahmouresyhedie@gmail.com>
2025-05-30 14:13:13 +0330

    Wallet related APIs
    * handled viewing wallet
    
    * charging wallet api finished
    
    * fixed wallet viewing
    
    * getting all transactions done

---
+++ volare/accounts/views/walletView.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ..serializers.walletSerializer import WalletSerializer
+
+
+class WalletView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = WalletSerializer.from_account(request.user.account_id)
+        return Response(serializer.data)
