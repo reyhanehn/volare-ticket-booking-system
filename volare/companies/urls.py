093950b676181262803ce6e468e9880a5c23c577
reyhaneh <reyhanehnemati1383@example.com>
2025-06-01 22:11:28 +0330

    get vehicle and company

--- volare/companies/urls.py
+++ volare/companies/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views.serviceView import ServiceListView, CreateServiceView
-from .views.companyView import CreateCompanyView
-from .views.vehicleView import CreateVehicleView
+from .views.companyView import CreateCompanyView, GetMyCompanyView, GetAllCompaniesView
+from .views.vehicleView import CreateVehicleView, GetAllVehiclesView
 
 urlpatterns = [
     path('services/', ServiceListView.as_view(), name='services'),
     path('admin/services/create/', CreateServiceView.as_view(), name='create_services'),
     path('admin/companies/create/', CreateCompanyView.as_view(), name='create-company'),
     path('admin/vehicles/create/', CreateVehicleView.as_view(), name='create-vehicle'),
+    path('my_company/', GetMyCompanyView().as_view(), name='my-company'),
+    path('admin/companies/list', GetAllCompaniesView.as_view(), name='list-companies'),
+    path('admin/vehicles/list', GetAllVehiclesView.as_view(), name='list-vehicles'),
 ]
