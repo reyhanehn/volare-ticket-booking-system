ad9afdcd647b82bb48e74418fdb3def0a7b348ce
HedieTahmouresi <tahmouresyhedie@gmail.com>
2025-06-02 06:06:11 +0330

    Routes handled
    * first draft of routes
    
    * fixed routes

--- volare/bookings/urls.py
+++ volare/bookings/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views.locationView import CreateLocationView, LocationListView
 from .views.stationView import CreateStationView, StationListView
+from .views.routeView import RouteListView, RouteCreateView
 
 urlpatterns = [
     path('admin/locations/create/', CreateLocationView.as_view(), name='create-location'),
     path('locations/list/', LocationListView.as_view(), name='location-list'),
     path('admin/stations/create/', CreateStationView.as_view(), name='create-station'),
     path('stations/list/', StationListView.as_view(), name='station-list'),
-
+    path('routes/list/', RouteListView.as_view(), name='routes-list'),
+    path('admin/routes/create/', RouteCreateView.as_view(), name='create-route'),
 ]
