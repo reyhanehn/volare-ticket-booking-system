5409785659eb121f03e3f0bf62d0422d0770417a
HedieTahmouresi <tahmouresyhedie@gmail.com>
2025-08-21 19:49:31 +0330

    settings

--- volare/volare/settings.py
+++ volare/volare/settings.py
@@ -62,7 +62,12 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5501",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 ROOT_URLCONF = 'volare.urls'
 
