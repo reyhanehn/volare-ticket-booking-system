365501f6c8dc3d09ec9e457c30bd9d99807e915c
HedieTahmouresi <tahmouresyhedie@gmail.com>
2025-08-22 00:29:36 +0330

    settings

--- volare/volare/settings.py
+++ volare/volare/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'volare',
         'USER': 'postgres',
-        'PASSWORD': 'HaHa1312!',
+        'PASSWORD': 'h.tah1584',
         'HOST': 'localhost',
         'PORT': '5432',
     }
